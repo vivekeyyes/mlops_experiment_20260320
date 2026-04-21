22475916d47c015fb673e8b9a3ee25b73971e778
vivek <vivek.sadarahalli@eyyes.com>
2026-04-21 07:38:29 +0200

    storage location update

--- scripts/eval.py
+++ scripts/eval.py
@@ -37,14 +37,16 @@ warnings.filterwarnings("ignore", message="Corrupt JPEG data")
 ######### CONFIG ############
 
 # mlflow config
-run_name = "20260421 demo run 2"
-experiment_name = "demo eval 20260421"
+run_name = "20260421 demo run"
+experiment_name = "20260421 demo eval"
 
-backend_store_uri = "file:///D:/Automation_pipeline/Full_Pipeline/20260421/artifacts/mlruns_eval/mlruns"
-default_artifact_root = "file:///D:/Automation_pipeline/Full_Pipeline/20260421/artifacts/mlflow_artifacts/Eval_runs/"+run_name
+
+backend_store_uri = "file:///D:/Automation_pipeline/Full_Pipeline/Artifact_storage/20260421/mlruns_eval/mlruns"
+default_artifact_root = "file:///D:/Automation_pipeline/Full_Pipeline/Artifact_storage/20260421/Eval_runs/"+run_name
 new_artifact_root = default_artifact_root
 
-base_artifact_path = "D:/Automation_pipeline/Full_Pipeline/20260421/artifacts/mlflow_artifacts/Training_runs/" + run_name
+# training run path to load model for evaluation
+base_artifact_path = "D:/Automation_pipeline/Full_Pipeline/Artifact_storage/20260421/Training_runs/" + run_name
 
 data_repo_path = r"D:\Automation_pipeline\Full_Pipeline\Dataset\Test_data"
 
